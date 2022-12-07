416aab8a8a174280e2fd293b8b427279c1e0e024
AK <akkhan07420@gmail.com>
2022-12-07 19:48:08 +0500

    fix url open problem

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ clicker.forEach((item) => {
         const target = e.target.innerText.toLowerCase();
         switch (target) {
             case "heart":
-                window.open(document.URL.replace("index.html", "shapes/heart/heart.html"));
+                window.open(document.URL + "shapes/heart/heart.html");
             case "triangle":
-                window.open(document.URL.replace("index.html", "shapes/triangle/triangle.html"))
+                window.open(document.URL + "shapes/triangle/triangle.html")
         }
     })
 })
